92b911e74bde544c2948b041b53a377fcbb41af2
Rafael Renck <rafaelrenck@gmail.com>
2021-11-29 20:06:22 -0300

    update repository to work at home

--- src/modules/sigh/routes/sigh.routes.ts
+++ src/modules/sigh/routes/sigh.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { Request, Response } from 'express';
-import { parseISO, startOfMonth, endOfMonth } from 'date-fns';
+import { startOfMonth, endOfMonth } from 'date-fns';
 
 import { knexDB as knex } from '../../../database/knex';
 
@@ -19,42 +19,86 @@ sighRoutes.get('/health_insurances', (request: Request, response: Response) => {
     });
 });
 
-sighRoutes.get('/appointments', (request: Request, response: Response) => {
-  const types = [];
+sighRoutes.get(
+  '/appointments',
+  async (request: Request, response: Response) => {
+    const types = [];
 
-  request.query.amb === 'true' && types.push('AMB');
-  request.query.ext === 'true' && types.push('EXT');
-  request.query.int === 'true' && types.push('INT');
+    const month = new Date(request.query.month.toString());
+    console.log(month);
+    console.log(startOfMonth(month));
+    console.log(endOfMonth(month));
 
-  const month = new Date(request.query.month.toString());
+    request.query.amb === 'true' && types.push('AMB');
+    request.query.ext === 'true' && types.push('EXT');
+    request.query.int === 'true' && types.push('INT');
 
-  knex
-    .select(
-      'fia.id_fia as id_fia',
-      'fia.data_atendimento as date',
-      'fia.hora_inicio as hour',
-      'fia.tipo_atend as type',
-      'pac.registro as id_patient',
-      'pac.nm_paciente as patient',
-      'pac.data_nasc as date_of_birth'
-    )
-    .from('sigh.ficha_amb_int as fia')
-    .leftJoin('sigh.pacientes as pac', 'fia.cod_paciente', 'pac.id_paciente')
-    .whereBetween('fia.data_atendimento', [
-      startOfMonth(month),
-      endOfMonth(month),
-    ])
-    .where('fia.cod_convenio', request.query.insurance.toString())
-    .whereIn('fia.tipo_atend', types)
-    .orderBy([
-      'fia.tipo_atend',
-      'fia.data_atendimento',
-      'pac.nm_paciente',
-      'fia.hora_inicio',
-    ])
-    .then((results) => {
-      return response.json(results);
-    });
-});
+    if (request.query.invoice === '') {
+      await knex
+        .select(
+          'fia.id_fia as id_fia',
+          'fia.data_atendimento as date',
+          'fia.hora_inicio as hour',
+          'fia.tipo_atend as type',
+          'pac.registro as id_patient',
+          'pac.nm_paciente as patient',
+          'pac.data_nasc as date_of_birth'
+        )
+        .from('sigh.ficha_amb_int as fia')
+        .leftJoin(
+          'sigh.pacientes as pac',
+          'fia.cod_paciente',
+          'pac.id_paciente'
+        )
+        .whereBetween('fia.data_atendimento', [
+          startOfMonth(month),
+          endOfMonth(month),
+        ])
+        .where({
+          'fia.cod_convenio': request.query.insurance.toString(),
+        })
+        .whereIn('fia.tipo_atend', types)
+        .whereRaw('pac.nm_paciente ilike ?', [
+          request.query.patient.toString().concat('%'),
+        ])
+        .orderBy([
+          'fia.tipo_atend',
+          'fia.data_atendimento',
+          'pac.nm_paciente',
+          'fia.hora_inicio',
+        ])
+        .then((results) => {
+          return response.json(results);
+        });
+    } else {
+      await knex
+        .select(
+          'fia.id_fia as id_fia',
+          'fia.data_atendimento as date',
+          'fia.hora_inicio as hour',
+          'fia.tipo_atend as type',
+          'pac.registro as id_patient',
+          'pac.nm_paciente as patient',
+          'pac.data_nasc as date_of_birth'
+        )
+        .from('sigh.faturas_contas as rem')
+        .leftJoin('sigh.contas as cnt', 'rem.id_fatura', 'cnt.cod_fatura')
+        .leftJoin('sigh.ficha_amb_int as fia', 'cnt.cod_fia', 'fia.id_fia')
+        .leftJoin(
+          'sigh.pacientes as pac',
+          'fia.cod_paciente',
+          'pac.id_paciente'
+        )
+        .where({
+          'rem.cod_convenio': request.query.insurance.toString(),
+          'rem.numero_fatura': request.query.invoice,
+        })
+        .orderBy(['pac.nm_paciente', 'fia.hora_inicio'])
+        .then((results) => {
+          return response.json(results);
+        });
+    }
+  }
+);
 
 export { sighRoutes };
